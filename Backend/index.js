5d6e345c053af91f4743a442860424d373f5b682
Aman singh <131010086+aman2282003@users.noreply.github.com>
2024-07-01 03:21:15 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,11 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin : ["https://fullstack-library-frontend.vercel.app/"],
+  methods : ["POST", "GET"],
+  credentials : true
+}));
 app.use(express.json());
 // const dotenv = require("dotenv").config()
 const PORT = process.env.PORT || 3000;
